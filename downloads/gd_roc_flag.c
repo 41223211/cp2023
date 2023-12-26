05d74fefbf2d6bf8db6f02a2d7341c5bf2560dcb
41223211 <144990816+41223211@users.noreply.github.com>
2023-12-26 07:51:35 +0000

    新增W15頁面

--- downloads/gd_roc_flag.c
+++ downloads/gd_roc_flag.c
@@ -3,19 +3,23 @@
 #include <math.h>
 
 void draw_roc_flag(gdImagePtr img);
-void draw_white_sun(gdImagePtr img, int center_x, int center_y, int sun_radius, int white, int red, int blue);
+void draw_white_sun(gdImagePtr img, int center_x, int center_y, int sun_radius, int color);
 
 int main() {
-    // width 3: height 2
     int width = 1200;
-    int height = (int)(width*2.0 / 3.0);
+    int height = (int)(width * 2.0 / 3.0);
 
     gdImagePtr img = gdImageCreateTrueColor(width, height);
     gdImageAlphaBlending(img, 0);
 
     draw_roc_flag(img);
 
-    FILE *outputFile = fopen("./../images/roc_flag.png", "wb");
+    int white = gdImageColorAllocate(img, 255, 255, 255);
+    gdImageLine(img, 429, 125, 279, 165, white);  // A to B
+    gdImageLine(img, 279, 165, 170, 274, white);  // B to E
+    gdImageLine(img, 170, 274, 429, 125, white);  // E to D
+
+    FILE *outputFile = fopen("roc_flag_in_gd.png", "wb");
     if (outputFile == NULL) {
         fprintf(stderr, "Error opening the output file.\n");
         return 1;
@@ -30,54 +34,46 @@ void draw_roc_flag(gdImagePtr img) {
     int width = gdImageSX(img);
     int height = gdImageSY(img);
     int red, white, blue;
-    int center_x = (int)(width/4);
-    int center_y = (int)(height/4);
-    int sun_radius = (int)(width/8);
+    int center_x = (int)(width / 4);
+    int center_y = (int)(height / 4);
+    int sun_radius = (int)(width / 8);
+    int white_circle_dia = sun_radius;
+    int blue_circle_dia = white_circle_dia + white_circle_dia * 2 / 15;
 
-    // Colors for the flag
-    red = gdImageColorAllocate(img, 242, 0, 0); // Red color
-    white = gdImageColorAllocate(img, 255, 255, 255); // White stripes
-    blue = gdImageColorAllocate(img, 0, 41, 204); // Blue
+    red = gdImageColorAllocate(img, 255, 0, 0);
+    white = gdImageColorAllocate(img, 255, 255, 255);
+    blue = gdImageColorAllocate(img, 0, 0, 149);
 
-    // 繪製紅色矩形區域
     gdImageFilledRectangle(img, 0, 0, width, height, red);
+    gdImageFilledRectangle(img, 0, 0, (int)(width / 2.0), (int)(height / 2.0), blue);
 
-    // 繪製藍色矩形區域
-    gdImageFilledRectangle(img, 0, 0, (int)(width/2.0), (int)(height/2.0), blue);
+    draw_white_sun(img, center_x, center_y, sun_radius, white);
 
-    // 繪製太陽
-    draw_white_sun(img, center_x, center_y, sun_radius, white, red, blue);
+    gdImageFilledEllipse(img, center_x, center_y, blue_circle_dia, blue_circle_dia, blue);
+    gdImageFilledEllipse(img, center_x, center_y, white_circle_dia, white_circle_dia, white);
 }
-void draw_white_sun(gdImagePtr img, int center_x, int center_y, int sun_radius, int white, int red, int blue) {
-    float angle = 0;
-    int numRays = 12; // 光芒的數量
 
-    gdPoint points[3]; // 三個頂點的陣列
+void draw_white_sun(gdImagePtr img, int center_x, int center_y, int sun_radius, int color) {
+    float deg = M_PI / 180;
 
-    for (int i = 0; i < numRays; i++) {
-        angle = i * (2 * M_PI / numRays);
-        float x1 = center_x + cos(angle) * sun_radius;
-        float y1 = center_y + sin(angle) * sun_radius;
+    int ax = center_x;
+    int ay = center_y - sun_radius;
 
-        // 調整兩個底邊頂點的位置
-      float x2 = center_x + cos(angle + 0.35) * (sun_radius * 0.5);
-      float y2 = center_y + sin(angle + 0.35) * (sun_radius * 0.5);
-      float x3 = center_x + cos(angle - 0.35) * (sun_radius * 0.5);
-      float y3 = center_y + sin(angle - 0.35) * (sun_radius * 0.5);
+    gdPoint points[4];
+    points[0].x = ax + sun_radius * sin(30 * deg);
+    points[0].y = ay + sun_radius - sun_radius * cos(30 * deg);
 
-        // 設定多邊形的三個頂點
-        points[0].x = (int)x1;
-        points[0].y = (int)y1;
-        points[1].x = (int)x2;
-        points[1].y = (int)y2;
-        points[2].x = (int)x3;
-        points[2].y = (int)y3;
+    points[1].x = ax - sun_radius * tan(15 * deg) + sun_radius * 2 - sun_radius * 2 * cos(30 * deg);
+    points[1].y = ay + sun_radius * 2 * sin(30 * deg);
 
-        gdImageFilledPolygon(img, points, 3, white);
-    }
-  //外圈
-  gdImageFilledEllipse(img, center_x, center_y, sun_radius * 1.2, sun_radius * 1.2, blue);
+    points[2].x = ax - sun_radius * sin(30 * deg);
+    points[2].y = ay + sun_radius - (sun_radius - sun_radius * cos(30 * deg));
 
-    // 繪製太陽內部
-    gdImageFilledEllipse(img, center_x, center_y, sun_radius * 1.1, sun_radius * 1.1, white);
+    points[3].x = ax + sun_radius * 2 - sun_radius * 2 * cos(30 * deg);
+    points[3].y = ay - sun_radius * 2 * sin(30 * deg);
+
+    gdImageLine(img, points[0].x, points[0].y, points[1].x, points[1].y, color);
+    gdImageLine(img, points[1].x, points[1].y, points[2].x, points[2].y, color);
+    gdImageLine(img, points[2].x, points[2].y, points[3].x, points[3].y, color);
+    gdImageLine(img, points[3].x, points[3].y, points[0].x, points[0].y, color);
 }
