5613bd4eec8e1c497d7516f78582f94c01f1be44
41223211 <144990816+41223211@users.noreply.github.com>
2023-12-27 16:10:38 +0000

    新增7

--- downloads/c7.c
+++ downloads/c7.c
@@ -1,3 +1,14 @@
+//#include <stdio.h>將sidio.h的檔案內容加進來
+//int main()
+//int radius半徑
+//int area面積
+//perimeter周長
+//perimeter = 2*3.14*radius;計算圓周
+//{}表示一個區塊的開始和結束
+//printf()把框框內的文字列印到螢幕上
+//;分號代表句子的結束
+//\n表示換行
+//return(0);回傳0這個值
 #include <stdio.h>
 int main(){
 int radius;
