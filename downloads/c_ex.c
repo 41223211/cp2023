15c220c6e9b7fa5ff6e4a89bb87e4a037622f797
41223211 <144990816+41223211@users.noreply.github.com>
2023-12-28 07:05:21 +0000

    新增期末總結

---
+++ downloads/c_ex.c
@@ -0,0 +1,9 @@
+//#include <stdio.h>printf這個函數定義放在stdio.h裡面
+//main() 這個 function 傳回(return)一個整數的數值, 執行這個程式在結束時, 會將這個整數的數值傳回給作業系統
+//printf("Hello World");在螢幕上印出
+#include <stdio.h>
+int main()
+{
+  printf("Hello World");
+  return(0);
+}
