e1bc59e7b26ff54e33298ad32b755030d9982786
41223211 <144990816+41223211@users.noreply.github.com>
2023-12-28 13:36:27 +0000

    新增2-2

---
+++ downloads/c_ex2.c
@@ -0,0 +1,15 @@
+//int a,A,b,c;宣告變數
+//a=1;A=8;b=2;左邊值填入右邊
+//c=A-b+c;計算出的值放入c
+//printf("%d",c);再把值印出來
+#include <stdio.h>
+int main(){
+  int a,A,b,c;
+  a=1;
+  A=8;
+  b=2;
+  c=A-b+c;
+  printf("%d",c);
+  return(0);
+  
+}
