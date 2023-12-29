bc924e2e8ec199f476057cf6332502751e72c7f4
41223211 <144990816+41223211@users.noreply.github.com>
2023-12-29 13:43:31 +0000

    新增2-7

---
+++ downloads/c_ex7.c
@@ -0,0 +1,11 @@
+//printf("Input an integer:");輸入整數
+//scanf("%d",&i);存到i
+// printf("the number is %d\n",i);在印出來
+#include <stdio.h>
+int main(){
+  int i;
+  printf("Input an integer:");
+  scanf("%d",&i);
+  printf("the number is %d\n",i);
+  return(0);
+}
