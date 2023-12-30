6093597061694ed3c6abfaeea59d0ed80aa1b7ce
41223211 <144990816+41223211@users.noreply.github.com>
2023-12-30 15:15:34 +0000

    新增2-10

---
+++ downloads/c_ex10.c
@@ -0,0 +1,11 @@
+//if(a>4)如果a>4
+//printf("a is larger than 4.\n");才會把這行印出
+#include <stdio.h>
+int main(){
+  int a = 3;//改5
+  if(a>4){
+    printf("a is larger than 4.\n");
+    
+  }
+  return(0);
+}
