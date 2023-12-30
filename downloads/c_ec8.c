23730d1ef74ca6bd2fd590bfa4ee16a10fef998e
41223211 <144990816+41223211@users.noreply.github.com>
2023-12-30 14:36:41 +0000

    新增2-8

---
+++ downloads/c_ec8.c
@@ -0,0 +1,14 @@
+//%顯示餘數,//顯示商數
+//printf("%d\n",a*b);把結果印出
+#include <stdio.h>
+int main(){
+  int a,b;
+  a=10;
+  b=3;
+  printf("%d\n",a*b);
+  printf("%d\n",a/b);
+  printf("%d\n",a+b);
+  printf("%d\n",a-b);
+  printf("%d\n",a%b);
+  return(0);
+}
