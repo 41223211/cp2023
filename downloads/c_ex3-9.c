fda1d133ed88a75e93b554b09c1c4149b03b924e
41223211 <144990816+41223211@users.noreply.github.com>
2024-01-01 13:58:45 +0000

    新增3-9

---
+++ downloads/c_ex3-9.c
@@ -0,0 +1,10 @@
+//int 長度是 4. ,char b是1
+#include <stdio.h> 
+  void   main() 
+  { 
+  char   b; 
+    printf( " The size of int is %ld\n", sizeof(int) ); 
+         
+     printf( " The size of char a  is %ld\n", sizeof(b) ); 
+     } 
+      
