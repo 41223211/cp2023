0950502b45d1fc267ec17e362dad5fcec9475b01
41223211 <144990816+41223211@users.noreply.github.com>
2024-01-11 11:31:57 +0000

    修改ANSI10.編寫一個 C 程序，將指定的日期轉換為年、週和日內容

--- downloads/c_ex3-8.c
+++ downloads/c_ex3-8.c
@@ -1,9 +1,22 @@
 //&&!一樣是不等於
 //結果為真傳1,假傳0假傳0
-#include <stdio.h> 
- void  main() 
- { 
-  printf("%d\n", 1 && 3 ); 
-  printf("%d\n", 0 && 0 ); 
-  printf("%d\n", 2 && 2 ); 
- } 
+//#include <stdio.h> 
+// void  main() 
+ //{ 
+ // printf("%d\n", 1 && 3 ); 
+ // printf("%d\n", 0 && 0 ); 
+  //printf("%d\n", 2 && 2 ); 
+ //} 
+#include <stdio.h>
+int main()
+{
+  int days,years,weeks;
+  days = 1329;
+  years = days/365;
+  weeks = (days % 365)/7;
+  days = days - ((years*365) + (weeks*7));
+  printf("Years: %d\n",years);
+  printf("Weeks: %d\n",weeks);
+  printf("Days: %d\n",days);
+  return (0);
+}
